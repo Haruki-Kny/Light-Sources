37af8a97c8ad6357973ac3730b024c226d0da646
Haruki-Kny <harukikonii@gmail.com>
2021-11-10 16:43:16 +0900

    first commit

--- Light Sources/ViewController.swift
+++ Light Sources/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    let showLight = SCNNode()
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        showShape()
+        lightOn()
+        sceneView.session.run(configuration)
+    }
+    
+    func showShape() {
+        let sphere = SCNSphere(radius: 0.03)
+        sphere.firstMaterial?.diffuse.contents = UIColor.white
+        
+        let node = SCNNode()
+        node.geometry = sphere
+        node.position = SCNVector3(0.1, 0, 0)
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    func lightOn(){
+
+        showLight.light = SCNLight()
+        showLight.light?.type = .directional
+        showLight.light?.color = UIColor(white: 0.6, alpha: 1.0)
+        showLight.position = SCNVector3(0,0,0)
+        sceneView.scene.rootNode.addChildNode(showLight)
     }
 
-
+    @IBAction func temperatureChange(_ sender: UISlider) {
+        showLight.light?.temperature = CGFloat(sender.value)
+    }
+    @IBAction func intensityChange(_ sender: UISlider) {
+        showLight.light?.intensity = CGFloat(sender.value)
+    }
+    @IBAction func colorButton(_ sender: UIButton) {
+        //colorMe.backgroundColor = sender.backgroundColor
+        showLight.light?.color = sender.backgroundColor!
+    }
 }
 
